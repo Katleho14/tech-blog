b2619866fe49e6f4c22fcab98be5ea62b0803d19
Katleho14 <katlehomotloung394@gmail.com>
2025-06-25 08:25:09 +0200

    refactor: Update next.config.mjs for image remote patterns and enhance globals.css with Tailwind CSS utilities

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/techblogcloud/image/upload/**", // ✅ Correct account name
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
+
+
