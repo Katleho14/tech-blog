017dda05f2e5f42c27850fb31d86d9642c39f538
Katleho14 <katlehomotloung394@gmail.com>
2025-06-25 03:14:48 +0200

    refactor: remove unused UI components and related code
    - Deleted Button, GradientButton, SuperUser, UsersCard, AlertDialog, Card, Chart, Input, Progress, Select, Textarea, Toast, Toaster, and use-toast components.
    - Cleaned up associated imports and references in EditUser component.

--- src/components/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="flex justify-center items-center w-full h-screen">
-      <div className="col-span-3 animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-indigo-700" />
-    </div>
-  );
-};
-
-export default LoadingSpinner;
