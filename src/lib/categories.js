8420677bb79e056975aff87437ff006e63c96b8e
Katleho14 <katlehomotloung394@gmail.com>
2025-07-01 22:10:27 +0200

    Delete src directory

--- src/lib/categories.js
+++
@@ -1,7 +0,0 @@
-const capitalizeFirstLetter = (str) => {
-  const capitalWord = str.charAt(0).toUpperCase() + str.slice(1);
-  return capitalWord;
-};
-
-const categories = ["all", "tech", "tutorial", "shopping"];
-export { categories, capitalizeFirstLetter };
