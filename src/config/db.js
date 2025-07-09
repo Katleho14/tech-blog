bb7127e13f96c7d794b44138e2c9a27859ba853d
Katleho14 <katlehomotloung394@gmail.com>
2025-07-09 12:12:11 +0200

    Update database connection URI in db.js and add 'vercel' to cSpell configuration in VSCode settingsf

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
-    mongoose.connect(process.env.URI);
+    mongoose.connect(process.env.MONGODB_URI);
     const connection = mongoose.connection;
     connection.on("connected", () => {
-      console.log("Connected to DataBase Successfuly!");
+      console.log("Connected to DataBase successfully!");
     });
     connection.on("error", (err) => {
       console.log(err.message);
